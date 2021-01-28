f877897096e171993d5ad9c9d304f476730fc687
AristeoIbarra <aristeoibarra608@gmail.com>
2021-01-27 20:57:42 -0700

    ClsDictionary class implementation

---
+++ AddressBook/ClsDirectory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressBook
+{
+    class ClsDirectory : Utilities.ClsModule
+    {
+        public dsAddressBook.directoryRow data;
+        public string FirstName = "";
+        public string LastName = "";
+        public string Email = "";
+        public string PhoneNumber = "";
+
+        public override bool Add()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool GetData(DataGridView dgv)
+        {
+            throw new NotImplementedException();
+        }    
+    }
+}
